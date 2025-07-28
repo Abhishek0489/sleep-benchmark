e357b89823941a963af0e031ffb6a328399f976c
Kazishyam <shyamkumart70500@gmail.com>
2025-07-28 22:02:49 +0530

    background changes added

--- sleep Benchmark/frontend/src/context/supabaseClient.js
+++ sleep Benchmark/frontend/src/context/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const supabaseUrl = 'https://ectuojivwypfztsvqkbd.supabase.co'
-const supabaseKey = '';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjdHVvaml2d3lwZnp0c3Zxa2JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMDg1ODksImV4cCI6MjA2Nzc4NDU4OX0._JnPAC8i2jHBkBetkxeBKxVlwZcS-qAtyRJXv8w-1fE';
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
