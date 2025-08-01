290226984fd86dba3bb0d6c92a31677bf27691e9
Abhishek0489 <ashek4official@gmail.com>
2025-08-01 22:48:47 +0530

    added a secure way to implement api keys

--- sleep Benchmark/frontend/src/context/supabaseClient.js
+++ sleep Benchmark/frontend/src/context/supabaseClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 
 
 const supabaseUrl = 'https://ectuojivwypfztsvqkbd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjdHVvaml2d3lwZnp0c3Zxa2JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMDg1ODksImV4cCI6MjA2Nzc4NDU4OX0._JnPAC8i2jHBkBetkxeBKxVlwZcS-qAtyRJXv8w-1fE';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
--- sleep Benchmark/server/index.js
+++ sleep Benchmark/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config();
 
 //adding 1 new comment line
 const app = express();
